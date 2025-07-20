7ced5aec2a64d95990bc877479127fb892415989
dani <martinelli.daniele7@gmail.com>
2025-07-20 12:01:36 +0200

    fix: beat url

--- src/utils/beat.ts
+++ src/utils/beat.ts
@@ -5,7 +5,7 @@ import { env } from '../lib/env';
 export const startHeartbeat = () => {
   cron.schedule('* * * * *', async () => {
     try {
-      await axios.get(`${env.serverUrl}:${env.port}/heartbeat`);
+      await axios.get(`${env.serverUrl}/heartbeat`);
     } catch (err) {
       console.error('Heartbeat ping failed', err);
     }
